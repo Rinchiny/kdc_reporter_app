8c8f80f144e996561b7131c4a2e2a2dde9dc1cb3
Rinchin Iakovlev <rbyakovlev@alfabank.ru>
2021-02-06 15:16:12 +0500

    Initial commit

---
+++ src/main/java/ru/boost/charity/kdc_reporter_app/KdcReporterAppApplication.java
@@ -0,0 +1,13 @@
+package ru.boost.charity.kdc_reporter_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KdcReporterAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KdcReporterAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/boost/charity/kdc_reporter_app/KdcReporterAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.boost.charity.kdc_reporter_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KdcReporterAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
